93497c6861b7e931e65fdf53f9cdd7ea1512756b
KostenkoDS <kostenko.exec@gmail.com>
2025-02-22 16:52:00 +0200

    initial commit

---
+++ backend/src/main/java/com/example/shopster/ShopsterApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/shopster/ShopsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

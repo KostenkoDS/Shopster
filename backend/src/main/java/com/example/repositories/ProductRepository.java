7e36fdda764c5d2f852e89db060f5840f7df05c6
KostenkoDS <kostenko.exec@gmail.com>
2025-02-26 21:55:49 +0200

    fix: change name of method that returns stream of all products in ProductRepository, add SQL query

--- backend/src/main/java/com/example/repositories/ProductRepository.java
+++ backend/src/main/java/com/example/repositories/ProductRepository.java
@@ -1,14 +1,18 @@
 package com.example.repositories;
 
 import com.example.entities.Product;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
     @Override
     List<Product> findAll();
     Optional<Product> findProductById(Long id);
     List<Product> findProductsByCategoryId(Long id);
+    @Query("SELECT * FROM PRODUCTS")
+    Stream<Product> findAllProductsStream();
 }

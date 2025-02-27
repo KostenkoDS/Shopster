379d05ab272fa74bd433c3b4719e05e2f57c995b
KostenkoDS <kostenko.exec@gmail.com>
2025-02-27 22:31:25 +0200

    feat: create ProductService (WIP), add method for finding products by multiple ids in ProductRepository

--- backend/src/main/java/com/example/repositories/ProductRepository.java
+++ backend/src/main/java/com/example/repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package com.example.repositories;
 import com.example.entities.Product;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +16,6 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     List<Product> findProductsByCategoryId(Long id);
     @Query("SELECT * FROM PRODUCTS")
     Stream<Product> findAllProductsStream();
+    @Query("SELECT * FROM products WHERE category_id IN (:ids)")
+    List<Product> findByMultipleIds(@Param("ids") List<Long> ids);
 }

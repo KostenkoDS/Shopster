29556028dd2b1ceb82a9a674b23a1d07b9044d50
Vol-khad <vol.khaduskin@gmail.com>
2025-03-02 13:38:34 +0100

    feat: add toggle behavior to show/hide filtering

--- frontend/src/home/priceFilter/component.jsx
+++ frontend/src/home/priceFilter/component.jsx
@@ -6,6 +6,11 @@ function PriceFilter({applyHandler}) {
 
     const [minPrice, setMinPrice] = useState(0);
     const [maxPrice, setMaxPrice] = useState(minPrice);
+    const [show, setShow] =useState(false);
+
+    const showHandler = () =>{
+        setShow((prevShow) => !prevShow)};
+
     const minPriceHandler = (value) => {
         setMinPrice((prevMinPrice) => (value>0&&value!==prevMinPrice?value:prevMinPrice));
     };
@@ -19,12 +24,14 @@ function PriceFilter({applyHandler}) {
 
     return(
 
-<div className="filter-container">Price:
-<div className="price-container">
-<input type="number" min = "0" className="min-price" onChange={minPriceHandler}></input> -
-<input type="number" className="max-price" min="0" onChange={maxPriceHandler}></input>
+<div className="filter-container">
+<div className="show" onClick={showHandler}>Price</div>
+{show?( <div className="price-container">
+<input type="number" min = "0" className="min-price" onChange={minPriceHandler} placeholder="0"></input> - 
+<input type="number" className="max-price" min="0" onChange={maxPriceHandler} placeholder="0"></input>
 <div className="ok-button" onClick={submit}>ok</div>
-</div>
+</div>):null}
+
 </div>
     )
 } 

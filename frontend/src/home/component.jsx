eb928d4ccaf4496ea9fbef0339b4f4ec028a255e
Vol-khad <vol.khaduskin@gmail.com>
2025-03-06 19:20:57 +0100

    feat: add fetching of products to main page

--- frontend/src/home/component.jsx
+++ frontend/src/home/component.jsx
@@ -1,11 +1,64 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import ProductList from "./productList/component";
 import './style.css'
 import PriceFilter from "./priceFilter/component";
 import CategoryFilter from "./categoryFilter/component";
 
 function Home (){
-   
+    const defaultProductsURL = 'api/products';
+    const [productsURL, setProductsUrl]  = useState (defaultProductsURL);
+    const productsReducer = (state, action)=>{
+       
+        switch(action.type){
+            case 'PRODUCTS_FETCH_INIT': 
+                return {
+                    ...state,
+                    isLoading:true,
+                    isError:false,
+                    success:false,
+                };
+            case 'PRODUCTS_FETCH_SUCCESS':
+                return{
+                    ...state,
+                    isLoading:false,
+                    isError:false,
+                    data:action.payload,
+                    success:true,
+                    
+                }
+            case 'PRODUCTS_FETCH_ERROR':
+                return{
+                    ...state,
+                    isLoading:false,
+                    success:false,
+                    isError:true,
+                }
+             default:
+                return state;   
+        }   
+    };
+    const [products, dispatchProducts] = useReducer(productsReducer,{
+        data:[], isLoading: false, isError:false, success:false
+    });
+
+    const handleFetchProducts=()=>{
+        dispatchProducts({type: 'PRODUCTS_FETCH_INIT'});
+            fetch(productsURL,{
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                }
+            })
+            .then((response)=>response.json())
+            .then((result)=>{
+                dispatchProducts({
+                  type: 'PRODUCTS_FETCH_SUCCESS',
+                  payload: result,
+                    }); })
+            .catch(()=> dispatchProducts({type:'PRODUCTS_FETCH_ERROR'})
+        )
+    }
+  
     const categories = [{
         id:1,
         name: 'CPU',
@@ -16,49 +69,23 @@ function Home (){
     }
 ]
 
-    const products = [{
-        id:1,
-        name: 'GeForce 2200',
-        price: 400.5,
-        description: 'Powerfull grafic cart of 5 generation', 
-        sequence: 1,
-        url: 'https://media.istockphoto.com/id/1295311673/es/foto/tarjeta-gr%C3%A1fica-del-juego-aislada-sobre-fondo-blanco-parte-de-la-computadora.jpg?s=2048x2048&w=is&k=20&c=oCTZovXKYJe2AJJpklrWA4FfKGLj8ZpFucDAfBr9LHg='
-      },
-    {
-      id:2,
-      name: 'GeForce 2200',
-      price: 400.5,
-      description: 'Powerfull grafic cart of 5 generation', 
-      sequence: 2,
-      url: 'https://media.istockphoto.com/id/1295311673/es/foto/tarjeta-gr%C3%A1fica-del-juego-aislada-sobre-fondo-blanco-parte-de-la-computadora.jpg?s=2048x2048&w=is&k=20&c=oCTZovXKYJe2AJJpklrWA4FfKGLj8ZpFucDAfBr9LHg='
-    },
-    {id:3,
-      name: 'GeForce 2200',
-      price: 400.5,
-      description: 'Powerfull grafic cart of 5 generation', 
-      sequence: 3,
-      url: 'https://media.istockphoto.com/id/1295311673/es/foto/tarjeta-gr%C3%A1fica-del-juego-aislada-sobre-fondo-blanco-parte-de-la-computadora.jpg?s=2048x2048&w=is&k=20&c=oCTZovXKYJe2AJJpklrWA4FfKGLj8ZpFucDAfBr9LHg='
-    },
+   const filterProductsPrice = (minPrice, maxPrice) =>{
+    if(maxPrice==0){
+        setProductsUrl(defaultProductsURL);
+    }
+    else{
+        const params = new URLSearchParams();
+        params.append("minPrice", minPrice);
+        params.append("maxPrice", maxPrice);
+        const newUrl = defaultProductsURL.concat('?'+params.toString());
+        setProductsUrl(newUrl);
+    }
+   }
 
-    {
-        id:4,
-        name: 'GeForce 2200',
-        price: 400.5,
-        description: 'Powerfull grafic cart of 5 generation', 
-        sequence: 3,
-        url: 'https://media.istockphoto.com/id/1295311673/es/foto/tarjeta-gr%C3%A1fica-del-juego-aislada-sobre-fondo-blanco-parte-de-la-computadora.jpg?s=2048x2048&w=is&k=20&c=oCTZovXKYJe2AJJpklrWA4FfKGLj8ZpFucDAfBr9LHg='
-      },
+useEffect(()=>{
+    handleFetchProducts();
+},[productsURL]);
 
-      {id:5,
-        name: 'GeForce 2200',
-        price: 400.5,
-        description: 'Powerfull grafic cart of 5 generation', 
-        sequence: 3,
-        url: 'https://media.istockphoto.com/id/1295311673/es/foto/tarjeta-gr%C3%A1fica-del-juego-aislada-sobre-fondo-blanco-parte-de-la-computadora.jpg?s=2048x2048&w=is&k=20&c=oCTZovXKYJe2AJJpklrWA4FfKGLj8ZpFucDAfBr9LHg='
-      },
-
-
-]
 
     return(
         <div className="home">
@@ -71,13 +98,14 @@ function Home (){
         </div>
         <div className="main">
             <div className="filter">
-            <PriceFilter/>
+            <PriceFilter applyHandler={filterProductsPrice}/>
             <CategoryFilter categories={categories}/>
         </div>
             <div className="product-list">
-            <ProductList products={products}/>
-            
-            </div>
+           {products.isLoading&&<p>Loading...</p>}
+           {products.isError&&<p>Something went wrong..</p>}
+           {products.success&&<ProductList products={products.data}/>}
+        </div>
         </div>
         <div className="futter">KHADUSKIN&KOSTENKO DEV</div>
         </div>

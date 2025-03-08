7b262eb560b06e9058894009831dad8c31b7fafc
Vol-khad <vol.khaduskin@gmail.com>
2025-03-08 14:37:13 +0100

    refactor: improve behavior of PriceFilter component

--- frontend/src/home/priceFilter/component.jsx
+++ frontend/src/home/priceFilter/component.jsx
@@ -3,7 +3,9 @@ import './style.css'
 
 
 function PriceFilter({applyHandler}) {
-
+    const parameters = new URLSearchParams(window.location.search);
+    let minPriceHolder = parameters.get("minPrice");
+    let maxPriceHolder = parameters.get("maxPrice");
     const [show, setShow] =useState(false);
     const [minPrice, setMinPrice] = useState('0');
     const [maxPrice, setMaxPrice] = useState('0');
@@ -26,12 +28,10 @@ function PriceFilter({applyHandler}) {
         } 
 
         if(Number(minPrice)>Number(maxPrice)) {
-            console.log(minPrice, maxPrice);
             window.alert("The maximum price of the product should be higher than the minimum price.")
             return;
         }
         else {
-            console.log(minPrice, maxPrice);
             setMaxPrice(maxPrice);
             setMinPrice(minPrice);
             applyHandler(minPrice,maxPrice);
@@ -44,8 +44,8 @@ function PriceFilter({applyHandler}) {
 <div className="show" onClick={showHandler}>Price</div>
 {show?( <div className="price-container">
 <form action={submit} className="priceForm">
-<input type="number" min = "0" className="min-price" name="minPriceValue" placeholder={minPrice}></input> - 
-<input type="number" className="max-price" min="0" name="maxPriceValue" placeholder={maxPrice}></input>
+<input type="number" min = "0" className="min-price" name="minPriceValue" placeholder={minPriceHolder}></input> - 
+<input type="number" className="max-price" min="0" name="maxPriceValue" placeholder={maxPriceHolder}></input>
 <button className="ok-button" type="submit">ok</button>
 </form>
 </div>):null}

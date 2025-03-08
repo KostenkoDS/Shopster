5e57f5591ed9135ded29ba91797e9dc3a5af1050
Vol-khad <vol.khaduskin@gmail.com>
2025-03-08 14:36:44 +0100

    refactor: improve behavior and style of CategoryFilter component

--- frontend/src/home/categoryFilter/component.jsx
+++ frontend/src/home/categoryFilter/component.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import './style.css';
+import Category from "./category";
 function CategoryFilter({categories,categoryFilterHandler}){
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(true);
+    const sortedCategories = categories.sort((a,b) =>a.name.length - b.name.length);
     
     const showHandler = () =>{
-            setShow((prevShow) => !prevShow)};
-            return(
+           
+            setShow((prevShow) => !prevShow);
+            console.log(show);
+        };
+            
+         return(
             <div className="categories-container" > 
-            <div className="show" onClick={showHandler} >Categories</div>
-           {show?categories.map(category=>(
-            <div className="category" key={category.id} onClick={()=>(categoryFilterHandler(category.id))}>{category.name}</div>
-                )):null}
-           </div>
-        )   
+                <div className="show" onClick={showHandler}>Categories</div>
+                {show?sortedCategories.map(category=>(
+                        <Category key ={category.id}  category = {category} categoryFilterHandler = {categoryFilterHandler}/>
+                )):null
+                }
+           </div>      
+         )   
 }
 
 export default CategoryFilter

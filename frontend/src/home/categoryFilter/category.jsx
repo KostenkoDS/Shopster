85dc103e6d795f371e948a9622f181dd19195ab0
Vol-khad <vol.khaduskin@gmail.com>
2025-03-08 14:35:21 +0100

    feat: create Category component for CategoryFilter

---
+++ frontend/src/home/categoryFilter/category.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Category({category, categoryFilterHandler}){
+    const parms = new URLSearchParams(window.location.search); 
+    const [isSelected, setIsSelected] = useState(false);
+   
+    const handleCategory = ()=> {
+            categoryFilterHandler(category.id); 
+    }
+
+    useEffect(()=>{
+        if(parms.has("c", category.id.toString())){
+            setIsSelected(true);
+        }
+        if(!parms.has("c",category.id.toString())) 
+            setIsSelected(false);
+    },[handleCategory])
+
+        return(
+        <div className="category" 
+            onClick={handleCategory} style={{color: isSelected&&"rgb(102, 227, 136)",
+                        fontWeight: isSelected&& "bold"
+            }}>{category.name}</div>
+        )
+}
+
+export default Category
+
+

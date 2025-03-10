3cf2bef2878b31c659a49bd3bfdd5a41048e3df4
KostenkoDS <kostenko.exec@gmail.com>
2025-03-10 21:52:40 +0200

    feat: add API for creating a user via POST /api/customer

--- backend/src/main/java/com/example/controllers/AccountController.java
+++ backend/src/main/java/com/example/controllers/AccountController.java
@@ -1,11 +1,12 @@
 package com.example.controllers;
 
 import com.example.dto.GeneralUserDTO;
+import com.example.dto.RegistrationDTO;
 import com.example.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AccountController {
@@ -15,6 +16,13 @@ public class AccountController {
 
     @GetMapping("/api/customer")
     GeneralUserDTO getCustomer(Authentication auth){
-        return customerService.findCustomerByEmail(auth.getName());
+        GeneralUserDTO u = customerService.findCustomerByEmail(auth.getName());
+        return u;
+    }
+
+    @PostMapping("/api/customer")
+    @ResponseStatus(HttpStatus.CREATED)
+    void createCustomer(@RequestBody RegistrationDTO data){
+        customerService.createCustomer(data);
     }
 }

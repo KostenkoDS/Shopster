98d77af299157e0d10827099419ccbd99ed86c54
KostenkoDS <kostenko.exec@gmail.com>
2025-03-10 21:49:56 +0200

    feat: change name of securityFilterChain method, enable csrf protection, set login endpoint to /api/login, change request matcher of /api/customer to include GET method

--- backend/src/main/java/com/example/config/WebSecurityConfiguration.java
+++ backend/src/main/java/com/example/config/WebSecurityConfiguration.java
@@ -2,29 +2,25 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration {
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.formLogin(Customizer.withDefaults());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+        http.formLogin(c -> c.loginPage("/api/login"));
         http.authorizeHttpRequests(
-                c -> c.requestMatchers("/api/customer").hasRole("CUSTOMER")
+                c -> c.requestMatchers(HttpMethod.GET ,"/api/customer").hasRole("CUSTOMER")
                         .anyRequest().permitAll()
         );
-        http.csrf(c -> c.disable());
         http.cors(c -> c.disable());
         return http.build();
     }

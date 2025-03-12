8b0cbd368784ff762f8a4e1aefaddad476f9c600
KostenkoDS <kostenko.exec@gmail.com>
2025-03-12 22:25:03 +0200

    feat: create DTO for Order and OrderDetails entities

---
+++ backend/src/main/java/com/example/dto/OrderDTO.java
@@ -0,0 +1,76 @@
+package com.example.dto;
+
+import com.example.entities.Order;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class OrderDTO {
+    Long id;
+    Long customerId;
+    LocalDate orderDate;
+    Order.Status status;
+    Set<OrderDetailsDTO> orderDetails;
+
+    public OrderDTO(@JsonProperty("id")
+                    Long id,
+                    @JsonProperty("customerId")
+                    Long customerId,
+                    @JsonProperty("orderDate")
+                    LocalDate orderDate,
+                    @JsonProperty("status")
+                    Order.Status status,
+                    @JsonProperty("orderDetails")
+                    Set<OrderDetailsDTO> orderDetails) {
+        this.id = id;
+        this.customerId = customerId;
+        this.orderDate = orderDate;
+        this.status = status;
+        this.orderDetails = orderDetails;
+    }
+
+    public OrderDTO(Order orderRecord, Map<Long, String> productNames){
+        this.id = orderRecord.getId();
+        this.customerId = orderRecord.getCustomerId();
+        this.orderDate = orderRecord.getOrderDate();
+        this.status = orderRecord.getStatus();
+        this.orderDetails = orderRecord.getOrderDetails().stream().map(od -> new OrderDetailsDTO(od, productNames.get(od.getOrderId()))).collect(Collectors.toSet());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public Order.Status getStatus() {
+        return status;
+    }
+
+    public Set<OrderDetailsDTO> getOrderDetails() {
+        return orderDetails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderDTO orderDTO = (OrderDTO) o;
+        return Objects.equals(getId(), orderDTO.getId()) && Objects.equals(getCustomerId(), orderDTO.getCustomerId()) && Objects.equals(getOrderDate(), orderDTO.getOrderDate()) && getStatus() == orderDTO.getStatus() && Objects.equals(getOrderDetails(), orderDTO.getOrderDetails());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getCustomerId(), getOrderDate(), getStatus(), getOrderDetails());
+    }
+}
---
+++ backend/src/main/java/com/example/dto/OrderDetailsDTO.java
@@ -0,0 +1,60 @@
+package com.example.dto;
+
+import com.example.entities.OrderDetails;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class OrderDetailsDTO {
+    final Long productId;
+    final String productName;
+    final Integer amount;
+    final BigDecimal price;
+
+    public OrderDetailsDTO(@JsonProperty("productId")
+                           Long productId,
+                           @JsonProperty("productName")
+                           String productName,
+                           @JsonProperty("amount")
+                           Integer amount,
+                           @JsonProperty("price")
+                           BigDecimal price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public OrderDetailsDTO(OrderDetails orderDetails, String productName) {
+        this.productId = orderDetails.getProductId();
+        this.productName = productName;
+        this.amount = orderDetails.getAmount();
+        this.price = orderDetails.getPrice();
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderDetailsDTO that = (OrderDetailsDTO) o;
+        return Objects.equals(getProductId(), that.getProductId()) && Objects.equals(productName, that.productName) && Objects.equals(getAmount(), that.getAmount()) && Objects.equals(getPrice(), that.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProductId(), productName, getAmount(), getPrice());
+    }
+}

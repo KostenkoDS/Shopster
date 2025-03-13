ac5a3a1d2a3d02cdd77449e78a77c88f1d94a7a2
KostenkoDS <kostenko.exec@gmail.com>
2025-03-13 21:50:35 +0200

    refactor: make temporary changes in web security config for development purposes

--- backend/src/main/java/com/example/config/WebSecurityConfiguration.java
+++ backend/src/main/java/com/example/config/WebSecurityConfiguration.java
@@ -3,12 +3,17 @@ package com.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -16,12 +21,27 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
-        http.formLogin(c -> c.loginPage("/api/login"));
+        http.formLogin(
+                c -> c.loginPage("/api/login")
+        );
         http.authorizeHttpRequests(
-                c -> c.requestMatchers(HttpMethod.GET ,"/api/customer").hasRole("CUSTOMER")
+                c -> c
+                        .requestMatchers(HttpMethod.GET ,"/api/customer").hasRole("CUSTOMER")
+                        .requestMatchers("/api/customer/orders").hasRole("CUSTOMER")
                         .anyRequest().permitAll()
         );
-        http.cors(c -> c.disable());
+        http.cors(c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(
+                        List.of("*"));
+                config.setAllowedMethods(
+                        List.of("GET", "POST", "PUT", "DELETE"));
+                config.setAllowedHeaders(List.of("*"));
+                return config;
+            };
+            c.configurationSource(source);
+        });
         return http.build();
     }
 

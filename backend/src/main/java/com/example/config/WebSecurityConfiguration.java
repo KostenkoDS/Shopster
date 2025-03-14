d220fb89edfeebe46959b6ff9f073b91fceac74b
KostenkoDS <kostenko.exec@gmail.com>
2025-03-14 22:29:00 +0200

    refactor: apply web security changes
    store CSRF tokens in cookies, generate new CSRF token every request,  return 200 OK status after successful authentication, add logout path

--- backend/src/main/java/com/example/config/WebSecurityConfiguration.java
+++ backend/src/main/java/com/example/config/WebSecurityConfiguration.java
@@ -1,18 +1,27 @@
 package com.example.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.*;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
 import java.util.List;
 
 @EnableWebSecurity
@@ -22,7 +31,12 @@ public class WebSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
         http.formLogin(
-                c -> c.loginPage("/api/login")
+                c -> c
+                        .loginPage("/api/login")
+                        .successHandler(new CustomAuthenticationSuccessHandler())
+        );
+        http.logout(
+                c -> c.logoutUrl("/api/logout")
         );
         http.authorizeHttpRequests(
                 c -> c
@@ -30,6 +44,12 @@ public class WebSecurityConfiguration {
                         .requestMatchers("/api/customer/orders").hasRole("CUSTOMER")
                         .anyRequest().permitAll()
         );
+        http.csrf(
+                c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+        );
+        http.addFilterAfter(new CsrfCookieFilter(), UsernamePasswordAuthenticationFilter.class);
         http.cors(c -> {
             CorsConfigurationSource source = request -> {
                 CorsConfiguration config = new CorsConfiguration();
@@ -49,5 +69,26 @@ public class WebSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    public static class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            Authentication authentication){
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+
+    public static final class CsrfCookieFilter extends OncePerRequestFilter {
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+                throws ServletException, IOException {
+            CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+            // Render the token value to a cookie by causing the deferred token to be loaded
+            csrfToken.getToken();
+
+            filterChain.doFilter(request, response);
+        }
     }
 }

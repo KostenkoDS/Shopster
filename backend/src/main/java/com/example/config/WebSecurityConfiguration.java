b5e80e15c27aa1734d240df25fca6a1e46bd96de
KostenkoDS <kostenko.exec@gmail.com>
2025-03-14 18:59:53 +0200

    refactor: change cors policy to allow credentials

--- backend/src/main/java/com/example/config/WebSecurityConfiguration.java
+++ backend/src/main/java/com/example/config/WebSecurityConfiguration.java
@@ -35,6 +35,7 @@ public class WebSecurityConfiguration {
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowedOrigins(
                         List.of("http://localhost:3000"));
+                config.setAllowCredentials(true);
                 config.setAllowedMethods(
                         List.of("GET", "POST", "PUT", "DELETE"));
                 config.setAllowedHeaders(List.of("*"));

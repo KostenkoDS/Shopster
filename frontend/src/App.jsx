798ba5f8231ee9a9a115d9206face5cbc68933d2
Vol-khad <vol.khaduskin@gmail.com>
2025-03-16 19:52:59 +0100

    feat: add protected routes in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,36 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import './App.css'
 import Home from './home/component'
 import SignUp from './auth/sign up/component'
 import Cart from './cart/component'
-import { Route, Router, Routes } from 'react-router-dom'
- 
+import { Navigate, Route, Router, Routes } from 'react-router-dom'
+import SignIn from './auth/sign in/component'
+import { useAuth } from './auth/authProvider/component'
+import SignOut from './auth/sign out/component'
 
 function App() {
+ 
+  const {user} = useAuth();
+  useEffect(()=>{
+  },[user]);
 
+  const ProtectedRoute = ({user, path, children})=>{
+    if(user===undefined){
+      console.log("PR" + user);
+      return <Navigate to = {path} replace/>
+    } 
 
+    else return children;
+  }
+    
   return (
   
       <Routes>
       <Route path="/" element ={ <Home/>}/>
+      <Route path='/auth/logout' element = {<SignOut/>}/>
       <Route path="/auth/sign-up" element={<SignUp/>}/>
-      <Route path='/cart' element ={<Cart/>}/>
+      <Route path ="/auth/sign-in" element={<SignIn/>}/>
+      <Route path='/cart' element = {<ProtectedRoute user={user} path="/auth/sign-in" children = {<Cart/>}/>}/>
       </Routes>
  
   )

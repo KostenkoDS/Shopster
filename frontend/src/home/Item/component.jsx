cdcdfaf5920eea3488475dab4fe79e05728e1079
Vol-khad <vol.khaduskin@gmail.com>
2025-03-20 18:50:18 +0100

    feat: implement addProductsToCart behavior

--- frontend/src/home/Item/component.jsx
+++ frontend/src/home/Item/component.jsx
@@ -1,16 +1,22 @@
+import { useOrderedProducts } from '../../cart/productContext';
 import styles from './item.module.css'
 function Item({product}){
+    const {addProductsToCart} = useOrderedProducts();
+    const handlerAddButton= ()=>{
+      addProductsToCart({id:product.id.toString(), amount:Number(1)});
+    }
+
     return(
        
         <div className={styles.itemContainer}>
-        <img src={product.url} className={styles.itemImg} ></img>
+        <img src={product.productPictures[1]} className={styles.itemImg} ></img>
         <div className={styles.desctiptionContainer}>
             <div className={styles.name}>{product.name}</div>
             <div className={styles.description}>{product.description}</div>
         </div>
         <div className={styles.purchaseContainer}>
             <div className={styles.price}>{product.price} $</div>
-            <button className={styles.addButton}>Add to cart</button>
+            <button className={styles.addButton} onClick={handlerAddButton}>Add to cart</button>
         </div>
         </div>
         

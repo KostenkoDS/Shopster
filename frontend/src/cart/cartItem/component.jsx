7870509f3c021d97e227dcc6db38cdb726917a91
Vol-khad <vol.khaduskin@gmail.com>
2025-03-20 18:44:34 +0100

    refactor: update behavior of decrementHandler and incrementHandler, add removeProductFromCart function

--- frontend/src/cart/cartItem/component.jsx
+++ frontend/src/cart/cartItem/component.jsx
@@ -1,50 +1,65 @@
+import { useOrderedProducts } from '../productContext';
 import styles from'./cartItem.module.css'
 import {useEffect, useState } from 'react';
 
-
-function CartItem({product, priceHandler}){
-
-const [amount, setAmount] = useState(1);
-const [totalPrice, setTotalPrice]= useState(product.price);
+function CartItem({product, priceHandler, removeProduct}){
+const {getAmount, addProductsToCart, deleteProductsFromCart} = useOrderedProducts();
+const [totalPrice, setTotalPrice]= useState(Number(product.price)*Number(getAmount({id:product.id})));
 
 const decrementHandler=()=>{
-    if(amount>1){
-        const newAmount = amount -1;
-        const newPrice = totalPrice - product.price;
-        setAmount(newAmount);
-        setTotalPrice(newPrice);
-        priceHandler(amount,product.price,'-');
-    }
-}
-const incrementHandler = ()=>{
-    const newAmount = amount +1;
-    const newPrice = totalPrice + product.price;
-        setAmount(newAmount);
-        setTotalPrice(newPrice);
-        priceHandler(amount,product.price,'+');
         
+    let amount = Number(getAmount({id:product.id}));
+    if(amount>1){
+       priceHandler(product.price,'-');
+       deleteProductsFromCart({id:product.id, amount:1});
+       let newPrice = (amount-1)*Number(product.price);
+       setTotalPrice(newPrice);
+    }
+     
+    if(amount === Number(1)){
+        let newPrice = product.price;
+        setTotalPrice(newPrice);
+    }
+    }
+
+const incrementHandler = ()=>{
+
+     addProductsToCart({id:product.id, amount:1});
+     let amount = Number(getAmount({id:product.id}));
+     let newPrice = (amount)*Number(product.price);
+     priceHandler(product.price,'+');
+     setTotalPrice(newPrice);       
 }
 
 useEffect(()=>{
-    priceHandler(amount,product.price,'+');
-    console.log(product.id);
-},[]);
+    let amount = Number(getAmount({id:product.id}));
+    priceHandler(amount*product.price,'+');
+},[])
+
+const removeProductFromCart = ()=>{
+    let amount = Number(getAmount({id:product.id}));
+    deleteProductsFromCart({id:product.id, amount:amount})
+    removeProduct({id:product.id});
+    priceHandler(product.price*amount,'-');
+ 
+}
 
     return(
         <div className={styles.container}>
             <div className={styles.descriptionContainer}>
             <div className={styles.product_img}>
-                <img className = {styles.img} src={product.url} alt={product.sequence}></img>
+                <img className = {styles.img} src={product.productPictures[1]   } alt={product.sequence}></img>
             </div>
                 <div className={styles.name}>{product.name}</div>
                 <div className={styles.description}>{product.description}</div>
+                <div className={styles.removeButton} onClick={removeProductFromCart}>X</div>
             </div>
             <div className={styles.futter}>
                 <div className={styles.productPricing}>        
                 <button className= {styles.buttonDec}  onClick={decrementHandler}>-</button>
-                <div className={styles.amount}>{amount}</div>
+                <div className={styles.amount}>{getAmount({id:product.id})}</div>
                 <button className= {styles.buttonInc} onClick={incrementHandler}>+</button>
-                <div className={styles.totalProductPrice}>{totalPrice} $</div>
+                <div className={styles.totalProductPrice}>{Number(totalPrice).toFixed(2)} $</div>
                 </div>
                 </div>     
                </div>

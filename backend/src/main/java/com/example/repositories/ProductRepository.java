0b4631a543745f2a9a08f0c555d82e8f75b05313
KostenkoDS <kostenko.exec@gmail.com>
2025-03-23 17:30:46 +0200

    feat: add more methods for returning only available products in ProductRepository, visually separate this kind of code from the code that returns from the general set of products

--- backend/src/main/java/com/example/repositories/ProductRepository.java
+++ backend/src/main/java/com/example/repositories/ProductRepository.java
@@ -8,19 +8,36 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
     @Override
     List<Product> findAll();
+
     Optional<Product> findProductById(Long id);
+
     @Query("SELECT * FROM products WHERE id IN (:ids)")
-    List<Product> findByMultipleProductIds(@Param("ids") List<Long> ids);
+    List<Product> findByMultipleProductIds(@Param("ids") Set<Long> ids);
+
     List<Product> findProductsByCategoryId(Long id);
-    @Query("SELECT * FROM PRODUCTS WHERE STOCK > 0")
-    Stream<Product> findAllAvailableProductsStream();
-    @Query("SELECT * FROM products WHERE category_id IN (:ids)")
-    Stream<Product> findByMultipleCategoryIds(@Param("ids") List<Long> ids);
+
     @Query("SELECT * FROM CATEGORIES")
     List<Category> findAllCategories();
+
+
+    /*
+    Methods below return only available products, that is where in-stock value is above zero
+     */
+
+    @Query("SELECT * FROM PRODUCTS WHERE CATEGORY_ID = :id AND STOCK > 0")
+    List<Product> findAvailableProductByCategoryId(@Param("id") Long id);
+
+    @Query("SELECT * FROM PRODUCTS WHERE STOCK > 0")
+    Stream<Product> findAllAvailableProductsStream();
+
+    @Query("SELECT * FROM products WHERE CATEGORY_ID IN (:ids) AND STOCK > 0")
+    Stream<Product> findAvailableProductsByMultipleCategoryIds(@Param("ids") Set<Long> ids);
+
+
 }

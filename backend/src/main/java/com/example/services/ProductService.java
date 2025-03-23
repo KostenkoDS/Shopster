4c33881bd533a06cf1d8dc1560339001b5c281d4
KostenkoDS <kostenko.exec@gmail.com>
2025-03-23 17:34:56 +0200

    feat: add more support for returning only available products in ProductService, change List params of ids to Set, change minPrice and maxPrice params type to Long, split the code into several sections

--- backend/src/main/java/com/example/services/ProductService.java
+++ backend/src/main/java/com/example/services/ProductService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -28,61 +29,79 @@ public class ProductService {
         return repository.findAll().stream().map(ProductDTO::new).toList();
     }
 
+    public ProductDTO findProductById(Long id){
+        Product p = repository.findProductById(id).orElseThrow();
+        return new ProductDTO(p);
+    }
+
+    public Map<Long,String> findProductNamesFromListOfOrders(List<Order> orderList){
+        Set<Long> productIdsInOrders = orderList
+                .stream()
+                .flatMap(order -> order.getOrderDetails().stream())
+                .map(OrderDetails::getProductId)
+                .collect(Collectors.toSet());
+        List<Product> products = repository.findByMultipleProductIds(productIdsInOrders);
+        return products.stream().collect(Collectors.toMap(Product::getId, Product::getName));
+    }
+
+    public List<ProductDTO> findProductsByIds(Set<Long> ids){
+        return repository.findByMultipleProductIds(ids).stream().map(ProductDTO::new).toList();
+    }
+
+
+    public Map<Long,String> findProductNamesFromOrder(Order order){
+        Set<Long> productIdsInOrder = order.getOrderDetails().stream().map(OrderDetails::getProductId).collect(Collectors.toSet());
+        return findProductsByIds(productIdsInOrder).stream().collect(Collectors.toMap(ProductDTO::getId, ProductDTO::getName));
+    }
+
+
+    /*
+    Methods in this section return only data about available products, that is where in-stock is above zero
+    */
+
     public List<ProductDTO> findAvailableProducts(){
         try(Stream<Product> stream = repository.findAllAvailableProductsStream()){
             return stream.map(ProductDTO::new).toList();
         }
     }
 
-    public List<ProductDTO> findProductsByCategoryId(Long id){
-        return repository.findProductsByCategoryId(id).stream().map(ProductDTO::new).toList();
+    public List<ProductDTO> findAvailableProductsByCategoryId(Long id){
+        return repository.findAvailableProductByCategoryId(id).stream().map(ProductDTO::new).toList();
     }
 
-    public ProductDTO findProductById(Long id){
-        Product p = repository.findProductById(id).orElseThrow();
-        return new ProductDTO(p);
+    public List<ProductDTO> findAvailableProductsByCategoryIds(Set<Long> ids){
+        return repository.findAvailableProductsByMultipleCategoryIds(ids).map(ProductDTO::new).toList();
     }
 
-    public List<ProductDTO> findProductsByIds(List<Long> ids){
-        return repository.findByMultipleProductIds(ids).stream().map(ProductDTO::new).toList();
-    }
-
-    public Map<Long,String> findProductNamesFromOrder(Order order){
-        List<Long> productIdsInOrder = order.getOrderDetails().stream().map(OrderDetails::getProductId).toList();
-        return findProductsByIds(productIdsInOrder).stream().collect(Collectors.toMap(ProductDTO::getId, ProductDTO::getName));
-    }
-
-    public Map<Long,String> findProductNamesFromListOfOrders(List<Order> orderList){
-        List<Long> productIdsInOrders = orderList
-                .stream()
-                .flatMap(order -> order.getOrderDetails().stream())
-                .map(OrderDetails::getProductId)
-                .toList();
-        List<Product> products = repository.findByMultipleProductIds(productIdsInOrders);
-        return products.stream().collect(Collectors.toMap(Product::getId, Product::getName));
-    }
-
-    public List<ProductDTO> findInAllProductsWithQueries(List<String> categories, String minPrice, String maxPrice, String name){
+    public List<ProductDTO> findAvailableProductsWithQueries(Set<Long> categories, Long minPrice, Long maxPrice, String name){
         if(categories == null){
             try (Stream<Product> stream = repository.findAllAvailableProductsStream()){
                 return findInStreamByPriceAndName(stream, minPrice, maxPrice, name).stream().map(ProductDTO::new).toList();
             }
         }
         else {
-            List<Long> categoryIds = categories.stream().map(Long::valueOf).distinct().toList();
-            try (Stream<Product> stream = repository.findByMultipleCategoryIds(categoryIds)){
+            try (Stream<Product> stream = repository.findAvailableProductsByMultipleCategoryIds(categories)){
                 return findInStreamByPriceAndName(stream, minPrice, maxPrice, name).stream().map(ProductDTO::new).toList();
             }
         }
     }
 
+
+    /*
+    This section is for the API for categories, methods here don't return product DTOs
+    */
+
     public List<CategoryDTO> findAllCategories(){
         return repository.findAllCategories().stream().map(CategoryDTO::new).toList();
     }
 
+    /*
+    Below are private to this class helper methods
+    */
+
     private List<Product> findInStreamByPriceAndName(Stream<Product> stream,
-                                                     String minPrice,
-                                                     String maxPrice,
+                                                     Long minPrice,
+                                                     Long maxPrice,
                                                      String name){
         return stream.filter(
                 p -> ((minPrice == null) || (p.getPrice().compareTo(new BigDecimal(minPrice)) >= 0)) &&
